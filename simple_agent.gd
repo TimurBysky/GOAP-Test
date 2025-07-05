53ffae840a37dd8c2f07ef28436bdca1132de8f9
Timur_Bysky <Timurbysky@gmail.com>
2025-07-05 22:59:43 +0300

    Анимация персонажа

--- simple_agent.gd
+++ simple_agent.gd
@@ -3,9 +3,11 @@ class_name CharacterBot
 
 @export var movement_speed: float = 4.0
 @onready var navigation_agent: NavigationAgent3D = get_node("NavigationAgent3D")
+@onready var animation: AnimationPlayer = get_node("Agent2/AnimationPlayer")
 
 func _ready() -> void:
 	navigation_agent.velocity_computed.connect(Callable(_on_velocity_computed))
+	animation.play("Idle")
 
 func set_movement_target(movement_target: Vector3):
 	navigation_agent.set_target_position(movement_target)
@@ -13,8 +15,10 @@ func set_movement_target(movement_target: Vector3):
 func _physics_process(delta):
 	# Do not query when the map has never synchronized and is empty.
 	if NavigationServer3D.map_get_iteration_id(navigation_agent.get_navigation_map()) == 0:
+		animation.play("Idle")
 		return
 	if navigation_agent.is_navigation_finished():
+		animation.play("Idle")
 		return
 
 	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
@@ -24,6 +28,7 @@ func _physics_process(delta):
 		navigation_agent.set_velocity(new_velocity)
 	else:
 		_on_velocity_computed(new_velocity)
+	animation.play("Walk")
 
 func _on_velocity_computed(safe_velocity: Vector3):
 	velocity = safe_velocity
